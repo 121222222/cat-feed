bad0bc7270de9a242438871034cca7ab4d81ad6e
rosyzxwang <rosyzxwang@tencent.com>
2026-03-20 13:57:16 +0800

    改金刚区，变大

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -8,32 +8,32 @@ Component({
       {
         pagePath: "/pages/index/index",
         text: "首页",
-        iconPath: "/assets/icons/home.svg",
-        selectedIconPath: "/assets/icons/home-active.svg"
+        iconPath: "/assets/icons/home.png",
+        selectedIconPath: "/assets/icons/home.png"
       },
       {
         pagePath: "/pages/service/service",
         text: "喂养服务",
-        iconPath: "/assets/icons/fish.svg",
-        selectedIconPath: "/assets/icons/fish-active.svg"
+        iconPath: "/assets/icons/fish.png",
+        selectedIconPath: "/assets/icons/fish.png"
       },
       {
         pagePath: "/pages/message/message",
         text: "消息",
-        iconPath: "/assets/icons/bell.svg",
-        selectedIconPath: "/assets/icons/bell-active.svg"
+        iconPath: "/assets/icons/bell.png",
+        selectedIconPath: "/assets/icons/bell.png"
       },
       {
         pagePath: "/pages/community/community",
         text: "社区",
-        iconPath: "/assets/icons/fire.svg",
-        selectedIconPath: "/assets/icons/fire-active.svg"
+        iconPath: "/assets/icons/fire.png",
+        selectedIconPath: "/assets/icons/fire.png"
       },
       {
         pagePath: "/pages/mine/mine",
         text: "我的",
-        iconPath: "/assets/icons/user.svg",
-        selectedIconPath: "/assets/icons/user-active.svg"
+        iconPath: "/assets/icons/user.png",
+        selectedIconPath: "/assets/icons/user.png"
       }
     ]
   },
